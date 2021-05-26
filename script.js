3ea065830c3b7c6492a85232a5d4b74528433867
Gatotz <gatot.prastyo@an.tv>
2021-05-27 05:18:16 +0700

    bismillah

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  // Activate Carousel
+  $("#myCarousel").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,  
+    slidesToShow:1,
+    slidesToScroll:1,
+    prevArrow:'<button type="button" class="slick-prev"></button>',
+    nextArrow:'<button type="button" class="slick-next"></button>',
+  });
+
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 5) {
+      $("nav").addClass("nav-bg");
+      $(".logo img.wt-logo").hide();
+      $(".logo img.fc-logo").show();
+      $(".nav-item a").addClass("black");
+    } else {
+      $("nav").removeClass("nav-bg");
+      $(".logo img.wt-logo").show();
+      $(".logo img.fc-logo").hide();   
+      $(".nav-item a").removeClass("black"); 
+    }
+  })
+    
+
+});
