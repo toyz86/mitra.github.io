fadad5fc238cb103a7dad7e5dc89410aebc08f16
Gatotz <gatot.prastyo@an.tv>
2021-05-27 07:22:37 +0700

    fix responssive

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ $(document).ready(function(){
   $(window).scroll(function() {
     if ($(document).scrollTop() > 5) {
       $("nav").addClass("nav-bg");
+      $(".menu-icon").addClass("menu-black");
       $(".logo img.wt-logo").hide();
       $(".logo img.fc-logo").show();
       $(".nav-item a").addClass("black");
     } else {
       $("nav").removeClass("nav-bg");
+      $(".menu-icon").removeClass("menu-black");
       $(".logo img.wt-logo").show();
       $(".logo img.fc-logo").hide();   
       $(".nav-item a").removeClass("black"); 
